929796f6dc9b6013e2505e4f95c53782e3f21e8d
avilajul <avilatas@gmail.com>
2023-09-03 01:45:49 -0500

    adicionar feature y runner

---
+++ src/test/java/co/com/company/certification/soap/runners/multiplicationResult.java
@@ -0,0 +1,15 @@
+package co.com.company.certification.soap.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/multiplies_two_integers.feature",
+        glue = "co.com.company.certificacion.soap",
+        snippets = SnippetType.CAMELCASE
+)
+public class multiplicationResult {
+}
---
+++ src/test/resources/features/multiplies_two_integers.feature
@@ -0,0 +1,15 @@
+#author: Julián Ávila
+
+Feature: multiply two integers
+  Scenario Outline: check the result of multiplying two integers
+  Scenario: successful outcome
+    Given julian entered the calculator page
+    When julian sends two positive integers
+    |multiplying  |multiplier  |
+    |<multiplying>|<multiplier>|
+    Then julian gets the expected result
+    |result  |
+    |<result>|
+    Examples:
+    |multiplying|multiplier|result|
+    |45         |3         |135   |
